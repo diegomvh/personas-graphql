a7ee1e052fcca1e25e183005c1ad9e78fa62db49
Diego van Haaster <diegomvh@gmail.com>
2016-07-18 08:00:37 -0300

    Personas full model

--- schemas/persona.js
+++ schemas/persona.js
@@ -3,8 +3,14 @@ var graphql = require('graphql');
 var PersonaType = new graphql.GraphQLObjectType({
   name: 'Persona',
   fields: {
-    id: { type: graphql.GraphQLString },
-    name: { type: graphql.GraphQLString },
+    documento: { type: graphql.GraphQLNumber },
+    clase: { type: graphql.GraphQLNumber },
+    apellidos: { type: graphql.GraphQLString },
+    nombres: { type: graphql.GraphQLString },
+    nombre_completo: { type: graphql.GraphQLString },
+    domicilio: { type: graphql.GraphQLString },
+    sexo: { type: graphql.GraphQLString },
+    tipo_documento: { type: graphql.GraphQLString }
   }
 });
 
